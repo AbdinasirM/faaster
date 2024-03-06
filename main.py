6378c4bc623a2a60343839735dd15d824717b089
Rasum771 <aydarusmohey12@gmail.com>
2024-03-06 00:18:50 -0600

    Added basic movements left and right plus jumping

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # Example file showing a basic pygame "game loop"
 import pygame 
-
+import Movements
+import time as t
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
-
+isJumping = False
+jumpCount = 10
+m = 1
 pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 while running:
+   #print(pos)
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -24,9 +28,34 @@ while running:
     
     # flip() the display to put your work on screen
     pygame.draw.circle(screen, "Purple", pos, 40) 
-    if pygame.key.get_pressed()[pygame.K_w]: 
-       #pygame.draw.circle(screen, "Purple", pos + (20,10), 40) 
-        pos.y += -10
+
+    Movements.BasicMove(pygame=pygame,pos=pos,t=t,isJumping=isJumping,jumpCount=jumpCount,m=m)
+
+    keys = pygame.key.get_pressed()
+    if not(isJumping):
+
+         if keys[pygame.K_w]:
+             isJumping = True
+    if isJumping: 
+        if jumpCount >= -10:
+            print(jumpCount)
+
+            F =(1 / 2)*m*(jumpCount**2)
+
+            pos.y -= F
+            
+            jumpCount = jumpCount - 1
+
+            if jumpCount < 0: 
+                m = -1
+            
+            if jumpCount == -11: 
+
+                isJumping = False
+                jumpCount = 10
+                m = 1
+
+    
     pygame.display.flip()
     
     clock.tick(60)  # limits FPS to 60
